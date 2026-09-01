99ac43f4f00995192380bc073acb07f9f85dd375
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-09-01 15:47:09 -0300

    Update sispnapa.py

--- sispnapa.py
+++ sispnapa.py
@@ -974,9 +974,9 @@ def validar_senha(senha_digitada: str, token_armazenado: str, senha_padrao="pnap
 # =================================================================
 st.markdown("""
     <style>
-        /* 1. BARRA LATERAL (Sidebar: #76993D) */
+        /* 1. BARRA LATERAL (Sidebar: #63783D) */
         section[data-testid="stSidebar"] {
-            background-color: #76993D !important;
+            background-color: #63783D !important;
         }
         section[data-testid="stSidebar"] h2,
         section[data-testid="stSidebar"] [data-testid="stWidgetLabel"] p,
